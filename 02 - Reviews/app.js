10ef5048b4dc8852be23198bc5cb857ea9d0734c
Anderson <andersonlsilva03@gmail.com>
2023-02-18 10:18:16 -0300

    Users Review Project

---
+++ 02 - Reviews/app.js
@@ -0,0 +1,69 @@
+// local reviews data
+const reviews = [
+  {
+    id: 1,
+    name: 'susan smith',
+    job: 'web developer',
+    img: 'https://images2.imgbox.com/e0/57/qI5bbwvg_o.jpeg',
+    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: 'anna johnson',
+    job: 'web designer',
+    img: 'https://images2.imgbox.com/2e/6e/JAMvTZ56_o.jpeg',
+    text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+  },
+  {
+    id: 3,
+    name: 'peter jones',
+    job: 'intern',
+    img: 'https://images2.imgbox.com/56/88/oJvFN3l5_o.jpeg',
+    text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+  },
+  {
+    id: 4,
+    name: 'bill anderson',
+    job: 'the boss',
+    img: 'https://images2.imgbox.com/8b/1c/vwWNTsCd_o.jpeg',
+    text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+  },
+];
+
+const img = document.getElementById('image');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const review = document.getElementById('review');
+
+const prevReview = document.getElementById('previousBtn')
+const nextReview = document.getElementById('nextBtn')
+
+let currentReview = 0;
+
+window.addEventListener('DOMContentLoaded', function(){
+  prevReview.addEventListener('click', showPreviousReview);
+  nextReview.addEventListener('click', showNextReview);
+  showReview()
+})
+
+function showReview (){
+  img.src = reviews[currentReview].img;
+  author.textContent = reviews[currentReview].name;
+  job.textContent = reviews[currentReview].job;
+  review.textContent = reviews[currentReview].text;
+}
+
+const showPreviousReview = () => {
+  currentReview--;
+  if(currentReview < 0) currentReview = 3
+  console.log(currentReview)
+  showReview()
+}
+
+const showNextReview = () => {
+  currentReview++;
+  if(currentReview > 3) currentReview = 0
+  console.log(currentReview)
+  showReview()
+}
+
