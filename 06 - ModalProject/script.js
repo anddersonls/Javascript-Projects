410d1b3e0fe816b29b582568373d92bf11a5e5c8
Anderson <andersonlsilva03@gmail.com>
2023-02-21 06:52:27 -0300

    Modal Project with animations

--- 06 - ModalProject/script.js
+++ 06 - ModalProject/script.js
@@ -4,15 +4,16 @@ const modalContent = document.querySelector('.modal_content')
 const closeModal = document.querySelector('.fa-times')
 
 openModal.addEventListener('click', function(){
-    modalContainer.classList.toggle('active')
-    modalContent.classList.toggle('is-active')
-    closeModal.classList.toggle('close')
+    modalContainer.classList.add('active')
+    modalContent.classList.add('is-active')
+    modalContent.classList.remove('upModal')
+    closeModal.classList.add('close')
 })
 
 closeModal.addEventListener('click', function(){
-    modalContainer.classList.toggle('active')
-    modalContent.classList.toggle('is-active')
-    modalContent.classList.toggle('upModal')
-    closeModal.classList.toggle('close') 
+    modalContainer.classList.remove('active')
+    modalContent.classList.remove('is-active')
+    modalContent.classList.add('upModal')
+    closeModal.classList.remove('close') 
 })
 
